71c32469a2534ef098d6f0c9b5355289f6c0269b
Nhung <nhung@Nhungs-MacBook-Pro.local>
2019-04-13 17:50:46 +0700

    setup project

---
+++ tests/acceptance/CrossTechCest.php
@@ -0,0 +1,13 @@
+<?php 
+
+class CrossTechCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(AcceptanceTester $I)
+    {
+    }
+}
